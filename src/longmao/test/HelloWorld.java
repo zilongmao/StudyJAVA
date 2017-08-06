6e42a438bb4e57fc7ca82bf8387e965266f96827
zilongmao <zilongmao@foxmail.com>
2017-08-07 05:18:02 +0800

    111

---
+++ src/longmao/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package longmao.test;
+
+public class HelloWorld {
+	public static void ShuChu(){
+		String syso = "HelloWorld!";
+		System.out.println(syso);
+	}
+	public static void main(String[] args) {
+		ShuChu();
+	}
+}
