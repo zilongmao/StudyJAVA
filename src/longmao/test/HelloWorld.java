d2145620cfc22b7df095a4afde14a37c0c6203f3
zilongmao <zilongmao@foxmail.com>
2017-08-07 06:00:33 +0800

    修改编码格式

--- src/longmao/test/HelloWorld.java
+++ src/longmao/test/HelloWorld.java
@@ -1,12 +1,12 @@
 package longmao.test;
 
 public class HelloWorld {
-	/** 输出的函数 */
+	/** 鍒涘缓杈撳嚭鍑芥暟 */
 	public static void ShuChu(){
 		String syso = "HelloWorld!";
 		System.out.println(syso);
 	}
-	/** 调用函数 */
+	/** 璋冪敤鍑芥暟 */
 	public static void main(String[] args) {
 		ShuChu();
 	}
