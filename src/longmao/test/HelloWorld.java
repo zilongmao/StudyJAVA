877a06538e990e9ccc77192451f067daef790477
zilongmao <zilongmao@foxmail.com>
2017-08-07 05:20:02 +0800

    添加了注释

--- src/longmao/test/HelloWorld.java
+++ src/longmao/test/HelloWorld.java
@@ -1,10 +1,12 @@
 package longmao.test;
 
 public class HelloWorld {
+	/** 输出的函数 */
 	public static void ShuChu(){
 		String syso = "HelloWorld!";
 		System.out.println(syso);
 	}
+	/** 调用函数 */
 	public static void main(String[] args) {
 		ShuChu();
 	}
